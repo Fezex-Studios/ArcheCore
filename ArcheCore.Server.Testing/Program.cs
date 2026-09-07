576a8bc66e452bb8c086800e7ea6b7f8ecb329bc
Thepipsqueak <98383696+Thepipsqueak62@users.noreply.github.com>
2026-09-06 21:19:51 -0400

    Added a test project for future persistence.server migration

---
+++ ArcheCore.Server.Testing/Program.cs
@@ -0,0 +1,10 @@
+﻿using AppLifetime.Example;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.UseExampleService();
+
+var host = builder.Build();
+host.Run();
