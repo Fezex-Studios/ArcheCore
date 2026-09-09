aecd4998644ab977180c43beca2c684b973a9960
Thepipsqueak <98383696+Thepipsqueak62@users.noreply.github.com>
2026-09-09 02:30:18 -0400

    Removed old persistence-server
    I'm moving away from the TypeScript PersistenceServer due to annoyance with cross language maintainability. long term i will use Asp.net with MessagePack. which is much better :) we don't need a custom TCP binary Packet system between worldserver and persistence server

--- src/ArcheCore.Server.Persistence/src/Shared/Interfaces/CharacterRow.ts
+++
@@ -1,9 +0,0 @@
-export interface CharacterRow {
-    character_id: number;
-    account_id:   number;
-    name:         string;
-    level:        number;
-    pos_x:        number;
-    pos_y:        number;
-    pos_z:        number;
-}
--- src/ArcheCore.Server.Persistence/src/Shared/Opcodes.ts
+++
@@ -1,9 +0,0 @@
-export enum ProtocolPersistence
-{
-    W2PConnectRequest = 1,
-    CharacterSave = 2,
-    CharacterLoad = 3,
-    P2WConnectResponse = 4,
-    P2WErrorResponse = 6,
-
-}
